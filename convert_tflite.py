fa5221711f09ee65393dc93679e01ed6f988ea02
SauravMaheshkar <sauravvmaheshkar@gmail.com>
2021-03-13 20:04:06 +0530

    Clean up Example Scripts

--- convert_tflite.py
+++ convert_tflite.py
@@ -2,9 +2,6 @@
 from config.config import CFG
 from forgetfuldnn.model.ForgetModel import Model
 
-# External
-import tensorflow as tf
-
 model = Model(CFG)
 model.build()
 model.load_pruned(weights="weights/model_ninety.h5", factor=0.9)
