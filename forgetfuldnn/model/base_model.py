c431d813ea85796a003a3d052e0299d58e43782e
SauravMaheshkar <sauravvmaheshkar@gmail.com>
2021-03-14 16:57:30 +0530

    Adds Library Code

---
+++ forgetfuldnn/model/base_model.py
@@ -0,0 +1,45 @@
+"""Abstract Base Model"""
+
+from abc import ABC, abstractmethod
+from forgetfuldnn.utils.config import Config
+
+
+class BaseModel(ABC):
+    """
+    Abstract Model class that is inherited to all models
+    """
+
+    def __init__(self, cfg):
+        self.config = Config.from_json(cfg)
+
+    @abstractmethod
+    def load_data(self):
+        pass
+
+    @abstractmethod
+    def build(self):
+        pass
+
+    @abstractmethod
+    def train(self):
+        pass
+
+    @abstractmethod
+    def load(self):
+        pass
+
+    @abstractmethod
+    def load_pruned(self):
+        pass
+
+    @abstractmethod
+    def export_tflite(self):
+        pass
+
+    @abstractmethod
+    def prune(self):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
